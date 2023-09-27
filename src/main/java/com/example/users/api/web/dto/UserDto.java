fe699f58c0bc06f113c9de2984e0ade8645f6bee
ashotvdovychenko <129285657+ashotvdovychenko@users.noreply.github.com>
2023-09-28 00:27:37 +0300

    Add basic user controller impl (#8)
    * Add necessary dependencies and plugins
    
    * Add DTO
    
    * Add UserMapper
    
    * Add UserController

---
+++ src/main/java/com/example/users/api/web/dto/UserDto.java
@@ -0,0 +1,15 @@
+package com.example.users.api.web.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+  private Long id;
+  private String firstName;
+  private String lastName;
+  private String username;
+  private String email;
+  private String birthDate;
+  private String address;
+  private String phoneNumber;
+}
