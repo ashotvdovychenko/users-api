9e4dcc4da4cf30884a79fb01d27198ae96208150
Ashot Vdovychenko <ashotvdovichenko@gmail.com>
2023-09-27 22:13:08 +0300

    Add User entity

---
+++ src/main/java/com/example/users/api/domain/User.java
@@ -0,0 +1,42 @@
+package com.example.users.api.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "USERS")
+public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "first_name")
+  private String firstName;
+
+  @Column(name = "last_name")
+  private String lastName;
+
+  private String username;
+
+  private String email;
+
+  private String password;
+
+  @Column(name = "birth_date")
+  private LocalDate birthDate;
+
+  private String address;
+
+  @Column(name = "phone_number")
+  private String phoneNumber;
+
+}
