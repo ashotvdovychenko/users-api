509cbe7253d47c58031d86b03d9e5a6cdf249a87
Ashot Vdovychenko <ashotvdovichenko@gmail.com>
2023-09-27 23:02:42 +0300

    Basic impl of UserService

--- src/main/java/com/example/users/api/repository/UserRepository.java
+++ src/main/java/com/example/users/api/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.example.users.api.repository;
 
 import com.example.users.api.domain.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+  Optional<User> findByUsername(String username);
 }
