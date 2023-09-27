808f79e795bda4597943902516589fb354b3ce81
Ashot Vdovychenko <ashotvdovichenko@gmail.com>
2023-09-28 02:36:26 +0300

    Add DTO schemas

--- src/main/java/com/example/users/api/web/dto/UserCreationDto.java
+++ src/main/java/com/example/users/api/web/dto/UserCreationDto.java
@@ -1,5 +1,6 @@
 package com.example.users.api.web.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -11,28 +12,36 @@ import lombok.Data;
 @Data
 public class UserCreationDto {
   @NotBlank(message = "Specify first name")
+  @Schema(example = "John")
   private String firstName;
 
   @NotBlank(message = "Specify last name")
+  @Schema(example = "Doe")
   private String lastName;
 
   @NotBlank(message = "Specify username")
   @Pattern(regexp = "^\\w+$", message = "You can use a-z, 0-9 and underscores")
   @Size(min = 4, max = 32, message = "Enter at least 4 and less than 32 characters")
+  @Schema(example = "string")
   private String username;
 
   @NotBlank(message = "Specify email")
   @Email(message = "Enter correct email")
+  @Schema(example = "string@mail.com")
   private String email;
 
   @NotBlank(message = "Specify password")
   @Size(min = 6, max = 32, message = "Enter at least 6 and less than 32 characters")
+  @Schema(example = "string")
   private String password;
 
   @NotNull(message = "Specify birth date")
+  @Schema(example = "2012-12-12")
   private LocalDate birthDate;
 
+  @Schema(example = "string")
   private String address;
 
+  @Schema(example = "+380123456789")
   private String phoneNumber;
 }
--- src/main/java/com/example/users/api/web/dto/UserUpdateDto.java
+++ src/main/java/com/example/users/api/web/dto/UserUpdateDto.java
@@ -1,5 +1,6 @@
 package com.example.users.api.web.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -9,25 +10,33 @@ import lombok.Data;
 @Data
 public class UserUpdateDto {
   @Pattern(regexp = "^[a-zA-Z-]+$", message = "You can use a-z and dashes")
+  @Schema(example = "John")
   private String firstName;
 
   @Pattern(regexp = "^[a-zA-Z-]+$", message = "You can use a-z and dashes")
+  @Schema(example = "Doe")
   private String lastName;
 
   @Pattern(regexp = "^\\w+$", message = "You can use a-z, 0-9 and underscores")
   @Size(min = 4, max = 32, message = "Enter at least 4 and less than 32 characters")
+  @Schema(example = "string")
   private String username;
 
   @Email(message = "Enter correct email")
   @Size(min = 5, message = "Enter at least 5 characters")
+  @Schema(example = "string")
   private String email;
 
+  @Schema(example = "2012-12-12")
   private LocalDate birthDate;
 
   @Size(min = 6, max = 32, message = "Enter at least 6 and less than 32 characters")
+  @Schema(example = "string")
   private String password;
 
+  @Schema(example = "string")
   private String address;
 
+  @Schema(example = "+380123456789")
   private String phoneNumber;
 }
