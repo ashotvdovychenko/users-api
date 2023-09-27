4b0f3ea1216c7cefc7a2f242fc3bc55c87fdcd7b
Ashot Vdovychenko <ashotvdovichenko@gmail.com>
2023-09-28 02:30:43 +0300

    Add Swagger support

--- src/main/java/com/example/users/api/UsersApiApplication.java
+++ src/main/java/com/example/users/api/UsersApiApplication.java
@@ -1,9 +1,18 @@
 package com.example.users.api;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+    info = @Info(title = "Users API", version = "0.1",
+        description = """
+            An API for the application implementing abilities to create, read, manage and other
+            general users manipulations."""
+    )
+)
 public class UsersApiApplication {
   public static void main(String[] args) {
     SpringApplication.run(UsersApiApplication.class, args);
--- src/main/java/com/example/users/api/web/controller/UserController.java
+++ src/main/java/com/example/users/api/web/controller/UserController.java
@@ -5,6 +5,10 @@ import com.example.users.api.web.dto.UserCreationDto;
 import com.example.users.api.web.dto.UserDto;
 import com.example.users.api.web.dto.UserUpdateDto;
 import com.example.users.api.web.mapper.UserMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.time.LocalDate;
 import java.util.List;
@@ -22,6 +26,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "User Controller")
 @RestController
 @RequestMapping(path = "/users", produces = MediaType.APPLICATION_JSON_VALUE)
 @RequiredArgsConstructor
@@ -31,6 +36,7 @@ public class UserController {
   private final UserMapper userMapper;
 
   @GetMapping(params = {"birth_date_from", "birth_date_to"})
+  @Operation(summary = "Get all users by birth date range")
   public ResponseEntity<List<UserDto>> findByBirthDateRange(@RequestParam(name = "birth_date_from")
                                                             LocalDate birthDateFrom,
                                                             @RequestParam(name = "birth_date_to")
@@ -42,17 +48,26 @@ public class UserController {
   }
 
   @GetMapping("/{id}")
+  @Operation(summary = "Get user by id", responses = {
+      @ApiResponse(responseCode = "200"),
+      @ApiResponse(responseCode = "404", content = @Content)
+  })
   public ResponseEntity<UserDto> findById(@PathVariable Long id) {
     return ResponseEntity.of(userService.findById(id).map(userMapper::toPayload));
   }
 
   @PostMapping
+  @Operation(summary = "Create user", responses = @ApiResponse(responseCode = "201"))
   public ResponseEntity<UserDto> create(@RequestBody @Valid UserCreationDto userDto) {
     var newUser = userService.create(userMapper.toEntity(userDto));
     return new ResponseEntity<>(userMapper.toPayload(newUser), HttpStatus.CREATED);
   }
 
   @PatchMapping("/{id}")
+  @Operation(summary = "Update user partially", responses = {
+      @ApiResponse(responseCode = "200"),
+      @ApiResponse(responseCode = "404", content = @Content)
+  })
   public ResponseEntity<UserDto> partialUpdate(@PathVariable Long id,
                                                @RequestBody @Valid UserUpdateDto userDto) {
     return ResponseEntity.of(userService.findById(id)
@@ -62,6 +77,8 @@ public class UserController {
   }
 
   @DeleteMapping("/{id}")
+  @Operation(summary = "Delete user by id",
+      responses = @ApiResponse(responseCode = "204", content = @Content))
   public ResponseEntity<Void> deleteById(@PathVariable Long id) {
     userService.deleteById(id);
     return ResponseEntity.noContent().build();
