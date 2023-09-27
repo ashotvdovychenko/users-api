3e7d69d456001a656b4bc68661171416c93324e4
ashotvdovychenko <129285657+ashotvdovychenko@users.noreply.github.com>
2023-09-27 22:19:47 +0300

    Add basic jpa entity (#3)
    * Add dependencies
    
    * Basic configuration
    
    * Project structure refactor
    
    * Add User entity
    
    * Add UserRepository

--- src/main/java/com/example/users/api/UsersApiApplication.java
+++ src/main/java/com/example/users/api/UsersApiApplication.java
@@ -1,4 +1,4 @@
-package com.example.usersapi;
+package com.example.users.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
