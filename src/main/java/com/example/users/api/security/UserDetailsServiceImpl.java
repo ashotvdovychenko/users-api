aa51d26092c198d2a16cc20c74189cbb596be9fc
Ashot Vdovychenko <ashotvdovichenko@gmail.com>
2023-09-28 14:03:07 +0300

    Add user details service

---
+++ src/main/java/com/example/users/api/security/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.example.users.api.security;
+
+import com.example.users.api.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+  private final UserRepository userRepository;
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    var user = userRepository.findByUsername(username).orElseThrow(
+        () -> new UsernameNotFoundException("User with username %s not found".formatted(username))
+    );
+    return new UserDetailsImpl(user);
+  }
+
+
+}
