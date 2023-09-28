1e35af848e366779883f95c49fc0f2bddc3d2601
ashotvdovychenko <129285657+ashotvdovychenko@users.noreply.github.com>
2023-09-28 04:23:43 +0300

    Add service level validation (#18)

--- src/main/java/com/example/users/api/service/UserServiceImpl.java
+++ src/main/java/com/example/users/api/service/UserServiceImpl.java
@@ -1,12 +1,15 @@
 package com.example.users.api.service;
 
 import com.example.users.api.domain.User;
+import com.example.users.api.exception.UserAlreadyExistsException;
 import com.example.users.api.repository.UserRepository;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,12 +18,22 @@ public class UserServiceImpl implements UserService {
   private final UserRepository userRepository;
 
   @Override
+  @Transactional
   public User create(User user) {
+    if (userRepository.existsByUsername(user.getUsername())) {
+      throw new UserAlreadyExistsException(
+          "Username %s is already in use".formatted(user.getUsername()));
+    }
     return userRepository.save(user);
   }
 
   @Override
+  @Transactional(isolation = Isolation.REPEATABLE_READ)
   public User update(User updatedUser) {
+    if (isUsernameInUse(updatedUser)) {
+      throw new UserAlreadyExistsException(
+          "Username %s is already in use".formatted(updatedUser.getUsername()));
+    }
     return userRepository.save(updatedUser);
   }
 
@@ -32,7 +45,8 @@ public class UserServiceImpl implements UserService {
   @Override
   public List<User> findAllByBirthDateRange(LocalDate birthDateFrom, LocalDate birthDateTo) {
     if (birthDateFrom.isAfter(birthDateTo)) {
-      throw new IllegalArgumentException();
+      throw new IllegalArgumentException(
+          "Date %s must be later than %s".formatted(birthDateTo, birthDateFrom));
     }
     return userRepository.findAllByBirthDateBetween(birthDateFrom, birthDateTo);
   }
@@ -45,5 +59,10 @@ public class UserServiceImpl implements UserService {
   @Override
   public void deleteById(Long id) {
     userRepository.deleteById(id);
+  }
+
+  private boolean isUsernameInUse(User user) {
+    return userRepository.findByUsername(user.getUsername())
+        .filter(found -> !found.getId().equals(user.getId())).isPresent();
   }
 }
