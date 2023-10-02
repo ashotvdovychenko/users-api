9b791a1b8e98e7aec9ef96964c98e075e09df788
ashotvdovychenko <129285657+ashotvdovychenko@users.noreply.github.com>
2023-10-02 03:33:21 +0300

    Add testcontainers (#35)
    * Add testcontainters
    
    * Edit workflows

--- src/test/java/com/example/users/api/controller/AuthControllerTests.java
+++ src/test/java/com/example/users/api/controller/AuthControllerTests.java
@@ -7,6 +7,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.example.users.api.repository.UserRepository;
+import com.example.users.api.testcontainers.TestcontainersInitializer;
 import com.example.users.api.web.dto.Credentials;
 import com.example.users.api.web.dto.UserCreationDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -17,11 +18,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
+@ContextConfiguration(initializers = TestcontainersInitializer.class)
 public class AuthControllerTests {
   private final String signUp = "/auth/sign-up";
 
